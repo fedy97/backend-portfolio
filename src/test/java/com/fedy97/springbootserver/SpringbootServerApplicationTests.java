93716b9d5a5cdf6edeb7b537d0637be5fc7340a0
fedy97 <frc.morreale@gmail.com>
2022-11-22 15:04:46 +0100

    first commit

---
+++ src/test/java/com/fedy97/springbootserver/SpringbootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedy97.springbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
