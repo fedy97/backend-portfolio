3f0df0f2898816956fb98c385bd8a503000d29ac
fedy97 <frc.morreale@gmail.com>
2022-11-25 17:07:58 +0100

    added some regex validation for Personal entity

--- src/main/java/com/fedy97/springbootserver/payload/request/PersonalRequest.java
+++ src/main/java/com/fedy97/springbootserver/payload/request/PersonalRequest.java
@@ -22,11 +22,15 @@ public class PersonalRequest {
     @NotNull(groups = {NewGroup.class})
     @Email(groups = {NewGroup.class, PatchGroup.class})
     private String email;
+    @Pattern(groups = {NewGroup.class, PatchGroup.class}, regexp = "^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$")
     private String phoneNumber;
+    @Pattern(groups = {NewGroup.class, PatchGroup.class}, regexp = "^(http(s):\\/\\/.)[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$")
     private String githubLink;
+    @Pattern(groups = {NewGroup.class, PatchGroup.class}, regexp = "^(http(s):\\/\\/.)[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$")
     private String linkedinLink;
     @Pattern(groups = {NewGroup.class, PatchGroup.class}, regexp = "^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$")
     private String birth;
+    @Pattern(groups = {NewGroup.class, PatchGroup.class}, regexp = "^(http(s):\\/\\/.)[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$")
     private String resumeLink;
     private String location;
     private String photo;
