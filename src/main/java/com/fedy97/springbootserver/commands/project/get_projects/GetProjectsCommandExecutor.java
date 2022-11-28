aab6f633d54c0da6e15a810686a549d47a0074e7
fedy97 <frc.morreale@gmail.com>
2022-11-28 14:16:54 +0100

    refactoring

--- src/main/java/com/fedy97/springbootserver/commands/project/get_projects/GetProjectsCommandExecutor.java
+++ src/main/java/com/fedy97/springbootserver/commands/project/get_projects/GetProjectsCommandExecutor.java
@@ -9,9 +9,6 @@ import com.fedy97.springbootserver.repositories.ProjectRepository;
 import com.fedy97.springbootserver.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
